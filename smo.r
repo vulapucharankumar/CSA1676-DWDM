28e0be56bb603f869b79252c966a3398745999ed
vulapucharankumar <113575655+vulapucharankumar@users.noreply.github.com>
2023-02-07 21:20:44 +0530

    Create smo.r

---
+++ smo.r
@@ -0,0 +1,27 @@
+BinarySMO
+
+Machine linear: showing attribute weights, not support vectors.
+
+        -0.5458 * (normalized) Day=D1
+ +       0.309  * (normalized) Day=D3
+ +       0.7465 * (normalized) Day=D4
+ +       0.2121 * (normalized) Day=D5
+ +      -1      * (normalized) Day=D6
+ +       0.2844 * (normalized) Day=D7
+ +      -0.7571 * (normalized) Day=D8
+ +       0.751  * (normalized) Day=D9
+ +      -0.5519 * (normalized) Outlook=Sunny
+ +       0.5934 * (normalized) Outlook=Overcast
+ +      -0.0414 * (normalized) Outlook=Rain
+ +      -0.2368 * (normalized) Temperature=Hot
+ +      -0.0106 * (normalized) Temperature=Mild
+ +       0.2474 * (normalized) Temperature=Cool
+ +       0.2474 * (normalized) Humidity=Normal
+ +      -0.7156 * (normalized) Wind=Strong
+ +       0.3026
+
+Number of kernel evaluations: 45 (88.402% cached)
+
+
+
+Time taken to build model: 0.03 seconds
